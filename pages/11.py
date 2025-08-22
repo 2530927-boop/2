243aaeccfc4a20d7ce6c60fced58e05a9822aee4
2530927-boop <2530927@sw.hs.kr>
2025-08-22 14:50:20 +0900

    Update 11.py

--- pages/11.py
+++ pages/11.py
@@ -4,3 +4,8 @@ st. title(어울리는 자동차 찾기)
 
 name = st.text_input('이름을 입력해주세요 : ')
 car = st.selectbox('자동차를 선택해주세요:', [
+  'BMW', 'Porsche', 'Hyundai', 'Aston_Martin', 'Mercedes', 'Lamborghini', 'Ferarri' ])
+
+mbti_data = {
+  'BMW': {
+    '특징': ' 처음에는 비행기용 엔진을 만드는 회사로 출범했으며 1923년에 첫 이륜차를 만들고 자동차를 처음 만든 것은 1929년이다
